3c392f19b748b5c0b2cef815cbefbad2abe070d4
dev.it <dev.it.nm@gmail.com>
2023-06-17 20:57:47 +0900

    Throttle

--- lib/screen/main/tab/home/w_toss_app_bar.dart
+++ lib/screen/main/tab/home/w_toss_app_bar.dart
@@ -1,14 +1,78 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 import '../../../../common/common.dart';
 
-class TossAppBar extends StatelessWidget {
+class TossAppBar extends StatefulWidget {
   final double appBarHeight;
 
   const TossAppBar({
     Key? key,
     required this.appBarHeight,
   }) : super(key: key);
+
+  @override
+  State<TossAppBar> createState() => _TossAppBarState();
+}
+
+class _TossAppBarState extends State<TossAppBar> {
+  StreamSubscription? _subscription;
+  final BehaviorSubject<bool> _isOn = BehaviorSubject.seeded(false);
+
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// TODO: Throttle time 1s
+    _subscription = _isOn.throttleTime(const Duration(seconds: 1)).listen(
+      (value) {
+        _listen(value);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    _isOn.close();
+
+    super.dispose();
+  }
+
+  Future<void> _listen(bool value) async {
+    try {
+      _count++;
+      debugPrint('count - $_count');
+
+      /// TODO: 가정 - 로그인 / 로그아웃, 2초 걸림
+      await Future.delayed(const Duration(seconds: 2));
+
+      /// 실패할 수도 있음.
+      if (Random().nextBool()) {
+        debugPrint('로그인 성공');
+      } else {
+        throw '로그인 실패';
+      }
+    } catch (e, s) {
+      /// 실패할 경우, 이전 값 되돌림.
+      _isOn.add(!value);
+
+      debugPrint('Error: $e, StackTrace: $s');
+    }
+
+    /// 완벽하게 에러를 막기 위한 방법
+    /// 1. dialog 같은 팝업 활용 (로그아웃을 하시겠습니까?)
+    /// 2. 네트워킹 bool 변수로 현재 API 요청 중이면, 무시하는 로직 추가
+  }
+
+  Future<void> _onTap() async {
+    _isOn.add(!_isOn.value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +82,27 @@ class TossAppBar extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           SizedBox(
-            height: appBarHeight,
+            height: widget.appBarHeight,
             child: Row(
               children: [
                 width5,
-                Image.asset(
-                  '$basePath/icon/toss.png',
-                  height: 30,
+                StreamBuilder<bool>(
+                  stream: _isOn.stream,
+                  builder: (context, snapshot) {
+                    final value = snapshot.data ?? false;
+
+                    return GestureDetector(
+                      onTap: () => _onTap(),
+                      behavior: HitTestBehavior.translucent,
+                      child: Opacity(
+                        opacity: value ? 1 : 0.5,
+                        child: Image.asset(
+                          '$basePath/icon/toss.png',
+                          height: 30,
+                        ),
+                      ),
+                    );
+                  },
                 ),
                 emptyExpanded,
                 Image.asset(
